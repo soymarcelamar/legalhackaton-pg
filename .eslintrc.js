d70b41742db4d39caa88bc37f30e58bc76ebf74c
DayanaM <dmmartinez269@misena.edu.co>
2020-11-26 10:49:16 -0500

    Config ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
+    "linebreak-style": 0,
   },
 };
